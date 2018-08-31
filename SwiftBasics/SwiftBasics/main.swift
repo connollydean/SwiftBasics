3bc552c06af1b966c7c91a5e8d7ceaac30ad0050
Connolly Dean <connollydean@Connollys-MacBook-Pro.local>
2018-08-31 17:58:55 -0500

    first commit

---
+++ SwiftBasics/SwiftBasics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftBasics
+//
+//  Created by Connolly Dean on 8/31/18.
+//  Copyright © 2018 Connolly Dean. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
